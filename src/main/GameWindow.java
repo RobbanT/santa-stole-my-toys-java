94318b77e17f376fb13b75643779151dd1a812b8
Roberto Tencic <roberto.tencic@outlook.com>
2023-11-12 16:22:04 +0100

    Update

---
+++ src/main/GameWindow.java
@@ -0,0 +1,14 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class GameWindow extends JFrame {
+
+    public GameWindow(GamePanel gamePanel) {
+        setSize(400, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(gamePanel);
+        setResizable(false);
+        setVisible(true);
+    }
+}
